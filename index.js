592a0afd6594f04059b421523cfcce64a69be921
ikhfil <ikhfilkhusen557@.com>
2023-12-08 11:29:44 +0700

    update existing

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-require ('dotenv').config();
-<<<<<<< HEAD
-=======
-
-/*const { Sequelize } = require('sequelize');
-const sequelize = require('./src/config/db_config.js');
-const auth = require('./src/routes/authRoutes.js');
-const router = require('./src/routes/authRoutes.js');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-require('dotenv').config();*/
->>>>>>> d8d29a6d707f0647e45210d11a59ed9e86073beb
 
 
 const app = express();
@@ -37,6 +25,6 @@ app.use('/api', postRoutes);
 app.use('/',hotelRoutes);
 app.use('/',destinasiRoutes);
 
-app.listen(process.env.PORT, () =>{
+app.listen(process.env.PORT,() =>{
   console.log('connection')
 })
