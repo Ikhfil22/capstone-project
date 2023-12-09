6f6f443927349979770d03c167c0361755510629
ikhfil <ikhfilkhusen557@.com>
2023-12-09 12:10:42 +0700

    update existing

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -8,7 +8,8 @@ require ('dotenv').config();
     process.env.DATABASE_PASSWORD, 
     {
       host: process.env.DATABASE_HOST,
-      dialect: 'mysql',
+      dialect: 'mysql', 
+      port: process.env.DATABASE_PORT,
     });
 
   sequelize.sync() 
